0b09314e52dc510194e54c7783d27078297d56dd
Ricardo <568082704@qq.com>
2024-01-21 06:13:10 +0800

    feat:更新

---
+++ Assets/Scrips/Destructiable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructiable : MonoBehaviour
+{
+    public int maxHp = 100;
+    private int currentHp;
+    public List<Sprite> injuredSpriteList;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+        print((int)(-0.5));
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHp = maxHp;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.relativeVelocity.magnitude);
+        if (currentHp < 0)
+        {
+            Dead();
+            return; 
+        }
+        else
+        {
+            currentHp -= (int)(collision.relativeVelocity.magnitude * 8);
+            int index = (int)(((maxHp - currentHp) / (maxHp / injuredSpriteList.Count + 1.0f))) - 1;
+            if (index >= 0 )
+            {
+                spriteRenderer.sprite = injuredSpriteList[index];
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
