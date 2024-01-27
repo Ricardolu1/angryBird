e7f1fdddcd8e2a85866107c3f91795b68a6dbf82
Ricardo <568082704@qq.com>
2024-01-28 04:53:49 +0800

    feat

--- Assets/Scrips/Boom.cs
+++ Assets/Scrips/Boom.cs
@@ -8,12 +8,9 @@ public class Boom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 1f);
+        Destroy(this.gameObject, 1.0f);
     }
 
     // Update is called once per frame
-    private void Update()
-    {   
-        
-    }
+   
 }
